25bd508267b637b88e1cdf23282a49318b3e3d80
abhijeet <20ucc003@lnmiit.ac.in>
2025-06-25 10:37:48 +0530

    shifted to models

--- app/views.py
+++ app/views.py
@@ -1,31 +1,35 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect , get_object_or_404
 
 # Create your views here.
+from  . models import Todo
 
-data  = {
 
-}
 
 
 def todos(request) :
-    context  =  {'data' :  data.items()}
+    all_todo = Todo.objects.all()
+    context  =  {'data' :  all_todo}
+    print(all_todo)
     return render(request ,'index.html'  , context )
 
 def add_todo(request) :
     if request.method  == 'POST' :
-        index  =  len(data) +1
         todo  =  request.POST.get('todo')
-        data[index] =  todo
+        todo_object  = Todo(todo  = todo)
+        todo_object.save()
         return  redirect('index')
 
 
 def update_todo(request  , id) :
     if request.method == "POST" :
         todo  =  request.POST.get('todo')
-        data[id] =  todo
+        todo_object  =  get_object_or_404(Todo ,  id = id)
+        todo_object.todo  =   todo
+        todo_object.save()
         return  redirect('index')
 
 
 def  delete_todo(request , id) :
-        del  data[id]
+        todo  = get_object_or_404(Todo ,  id  =  id )
+        todo.delete()
         return redirect('index')
